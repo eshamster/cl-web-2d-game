130770dfbec16b69e80186fc9c7c8fa9e019552f
eshamster <hamgoostar@gmail.com>
2016-08-14 14:19:21 +0000

    Generate from a template
    Use a "with_parenscript" template in
    [caveman-skeltons](https://github.com/eshamster/caveman-skeltons).

---
+++ README.markdown
@@ -0,0 +1,16 @@
+# cl-shigi-simulator
+
+
+
+## Usage
+
+## Installation
+
+## Author
+
+* eshamster (hamgoostar@gmail.com)
+
+## Copyright
+
+Copyright (c) 2016 eshamster (hamgoostar@gmail.com)
+
---
+++ t/cl-shigi-simulator.lisp
@@ -0,0 +1,12 @@
+(in-package :cl-user)
+(defpackage cl-shigi-simulator-test
+  (:use :cl
+        :cl-shigi-simulator
+        :prove))
+(in-package :cl-shigi-simulator-test)
+
+(plan nil)
+
+;; blah blah blah.
+
+(finalize)
