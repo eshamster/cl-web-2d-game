e1d7ead141d147ed9db45a8026e2aa54649808d4
eshamster <hamgoostar@gmail.com>
2016-09-28 16:24:57 +0000

    Enable and disable model-2d.

--- js-lib/draw-model-system.lisp
+++ js-lib/draw-model-system.lisp
@@ -23,13 +23,15 @@
 
 (enable-ps-experiment-syntax)
 
+;; This package is depend on three.js.
+
 ;; --- component --- ;;
 
 (defstruct.ps+ (model-2d (:include ecs-component))
     model
   (depth 0)
   (offset (make-point-2d))
-  (enable t))
+  (enable nil))
 
 ;; --- system --- ;;
 
@@ -48,11 +50,28 @@
                                  (model-2d-depth model-2d)) 
                                 (setf model.rotation.z (point-2d-angle new-pos))))))))))
 
+(defvar.ps *scene-for-draw-system* nil)
+
+;; TODO: Don't enabel if the entity is not registered in the draw-model-system.
+(defun.ps enable-model-2d (entity)
+  (unless *scene-for-draw-system*
+    (error "The scene for the draw system is not initialized"))
+  (with-ecs-components (model-2d) entity
+    (with-slots (model enable) model-2d
+      (unless enable
+        (*scene-for-draw-system*.add model)
+        (setf enable t)))))
+
+(defun.ps disable-model-2d (entity)
+  (unless *scene-for-draw-system*
+    (error "The scene for the draw system is not initialized"))
+  (with-ecs-components (model-2d) entity
+    (with-slots (model enable) model-2d
+      (*scene-for-draw-system*.remove model)
+      (setf enable nil))))
+
 (defun.ps init-draw-model-system (scene)
+  (setf *scene-for-draw-system* scene)
   (make-draw-model-system
-   :add-entity-hook (lambda (entity)
-                      (with-ecs-components (model-2d) entity
-                        (scene.add (model-2d-model model-2d))))
-   :delete-entity-hook (lambda (entity)
-                         (with-ecs-components (model-2d) entity
-                           (scene.remove (model-2d-model model-2d))))))
+   :add-entity-hook #'enable-model-2d
+   :delete-entity-hook #'disable-model-2d))
