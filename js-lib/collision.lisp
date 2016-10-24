a09fd2c32e73543d99bdbbcc5454f77c2c409f71
eshamster <hamgoostar@gmail.com>
2016-10-24 13:52:25 +0000

    Impelemnt collision between a circle and a polygon.
    And refactor the collision betweea a circle and a polygon as a special
    case of such the collision.

--- js-lib/collision.lisp
+++ js-lib/collision.lisp
@@ -19,9 +19,16 @@
            :physic-triangle
            :make-physic-triangle
 
+           :physic-polygon
+           :make-physic-polygon
+
            :collision-system
            :make-collision-system))
 (in-package :cl-web-2d-game.collision)
+
+#|
+Note: col-xx-vec takes 'point' and 'offset' for each physic. The 'point' means a global point of an entity that has the physic. Then, the 'offset' means a local offset in the entity.
+|#
 
 ;; --- components --- ;;
 
@@ -35,10 +42,14 @@
 (defstruct.ps+ (physic-triangle (:include physic-2d (kind :triangle)))
     (pnt1 (make-vector-2d)) (pnt2 (make-vector-2d)) (pnt3 (make-vector-2d)))
 
+(defstruct.ps+ (physic-polygon (:include physic-2d (kind :polygon)))
+  (pnt-list '()))
+
 ;; --- basic funcions --- ;;
 
 ;; c: Circle
 ;; t: Triangle
+;; p: Polygon (Only convex polygon)
 ;; r: Rectangle
 
 ;; - c to c - ;;
@@ -64,44 +75,86 @@
     (col-cc-vec point1 offset1 r1
                 point2 offset2 r2)))
 
+;; - c to p (po) - ;;
+;; Use 'po' as an abbreviation of 'polygon' because 'p' is confusing with 'p' that is used for boolean values or functions.
+
+(defun.ps+ col-cp (cx cy cr point-list-po)
+  "Do collision between a circle and a convec polygon. (The cx cy cr are circle parameters. The point-list-po is a polygon parameters (A list of vector-2d)
+Algorighm: A circle and a polygon collisions under following conditions
+  1. The center of the circle is in the polygon. <OR>
+  2. A line of the polygon intersects to the circle.
+     (This contains the case where the circle includes the line.)
+Note: The second condition can't check only the case where
+      any point of the circle is in the polygon. The first
+      condition can check such case."
+  (let ((num-pnts (length point-list-po)))
+    (when (= num-pnts 0)
+      (error "Can't do collision with an empty poligon."))
+    ;; The following pattern can be implemented, but have not.
+    (when (< num-pnts 3)
+      (error "Can't do collision with an point (length 1) or a line (length 2)."))
+    ;; Check the center of the circle is in the polygon.
+    (dotimes (i (- num-pnts 2))
+      (when (is-pnt-in-triangle
+             cx cy
+             (vector-2d-x (nth 0 point-list-po))
+             (vector-2d-y (nth 0 point-list-po))
+             (vector-2d-x (nth (+ i 1) point-list-po))
+             (vector-2d-y (nth (+ i 1) point-list-po))
+             (vector-2d-x (nth (+ i 2) point-list-po))
+             (vector-2d-y (nth (+ i 2) point-list-po)))
+        (return-from col-cp t)))
+    ;; Check a line of the polygon intersects to the circle.
+    (dotimes (i num-pnts)
+      (when (intersects-line-and-circle
+             cx cy cr
+             (vector-2d-x (nth i point-list-po))
+             (vector-2d-y (nth i point-list-po))
+             (vector-2d-x (nth (mod (1+ i) num-pnts) point-list-po))
+             (vector-2d-y (nth (mod (1+ i) num-pnts) point-list-po)))
+        (return-from col-cp t)))
+    nil))
+
+(defun.ps+ col-cp-vec (point-c offset-c rc point-po offset-po vec-list-po)
+  ;; TODO: Reduce memory allocations
+  (let ((global-point-c (clone-point-2d offset-c))
+        (global-point-list-po '()))
+    (transformf-point global-point-c point-c)
+    (dolist (vec vec-list-po)
+      (let ((point (clone-point-2d offset-po)))
+        (incf-vector point vec)
+        (transformf-point point point-po)
+        (push point global-point-list-po)))
+    (col-cp (point-2d-x global-point-c)
+            (point-2d-y global-point-c)
+            rc
+            global-point-list-po)))
+
+(defun.ps+ col-cp-physic (circle point-c polygon point-po)
+  (check-type circle physic-circle)
+  (check-type polygon physic-polygon)
+  (with-slots-pair (((offset-c offset) r) circle
+                    ((offset-po offset) pnt-list) polygon)
+    (col-cp-vec point-c offset-c r
+                point-po offset-po pnt-list)))
+
 ;; - c to t - ;;
 
-(defun.ps+ col-ct (cx cy cr
-                   tx1 ty1 tx2 ty2 tx3 ty3)
-  ;; A circle and a triangle collisions under following conditions
-  ;;   1. The center of the circle is in the triangle. <OR>
-  ;;   2. A line of the triangle intersects to the circle.
-  ;; Note: The second condition can't check only the case where
-  ;;       any point of the circle is in the triangle. The first
-  ;;       condition can check such case.
-  (or (is-pnt-in-triangle cx cy tx1 ty1 tx2 ty2 tx3 ty3)
-      (intersects-line-and-circle cx cy cr tx1 ty1 tx2 ty2)
-      (intersects-line-and-circle cx cy cr tx2 ty2 tx3 ty3)
-      (intersects-line-and-circle cx cy cr tx3 ty3 tx1 ty1)))
-
-;; TODO: the offset parameters are not well-tested.
 (defun.ps+ col-ct-vec (point-c offset-c rc
                                point-t offset-t vertex-t1 vertex-t2 vertex-t3)
   ;; TODO: Reduce memory allocations
-  (macrolet ((add-x (&rest point-list)
-               `(+ ,@(mapcar (lambda (point) `(vector-2d-x ,point)) point-list)))
-             (add-y (&rest point-list)
-               `(+ ,@(mapcar (lambda (point) `(vector-2d-y ,point)) point-list))))
-    (let ((buffer-pnts '()))
-      (dolist (vertex (list vertex-t1 vertex-t2 vertex-t3))
+  (let ((global-point-c (clone-point-2d offset-c))
+        (buffer-pnts '()))
+    (transformf-point global-point-c point-c)
+    (dolist (vertex (list vertex-t1 vertex-t2 vertex-t3))
         (let ((pnt (make-point-2d :x (vector-2d-x vertex) :y (vector-2d-y vertex))))
           (transformf-point pnt offset-t)
           (transformf-point pnt point-t)
           (push pnt buffer-pnts)))
-      (col-ct (add-x point-c offset-c)
-              (add-y point-c offset-c)
-              rc
-              (point-2d-x (nth 0 buffer-pnts))
-              (point-2d-y (nth 0 buffer-pnts))
-              (point-2d-x (nth 1 buffer-pnts))
-              (point-2d-y (nth 1 buffer-pnts))
-              (point-2d-x (nth 2 buffer-pnts))
-              (point-2d-y (nth 2 buffer-pnts))))))
+    (col-cp (point-2d-x global-point-c)
+            (point-2d-y global-point-c)
+            rc
+            buffer-pnts)))
 
 (defun.ps+ col-ct-physic (circle point-c triangle point-t)
   (check-type circle physic-circle)
@@ -147,6 +200,19 @@
                        (col-ct-physic ph1 pnt1 ph2 pnt2))
                       ((is-kind-pair ph1 ph2 :triangle :circle)
                        (col-ct-physic ph2 pnt2 ph1 pnt1))
+                      ((is-kind-pair ph1 ph2 :circle :polygon)
+                       (col-cp-physic ph1 pnt1 ph2 pnt2))
+                      ((is-kind-pair ph1 ph2 :polygon :circle)
+                       (col-cp-physic ph2 pnt2 ph1 pnt1))
+                      ;;--- TODO: Implement the followings
+                      ((is-kind-pair ph1 ph2 :triangle :polygon)
+                       nil)
+                      ((is-kind-pair ph1 ph2 :polygon :triangle)
+                       nil)
+                      ((is-kind-pair ph1 ph2 :polygon :polygon)
+                       nil)
+                      ((is-kind-pair ph1 ph2 :triangle :triangle)
+                       nil)
                       (t (error "not recognized physical type")))
             (with-slots-pair (((event1 on-collision)) ph1
                               ((event2 on-collision)) ph2)
