b386567b709638a0db8f0078df7b65b4cff59081
eshamster <hamgoostar@gmail.com>
2017-09-25 06:41:04 +0000

    Change a name from initialize-input to init-input
    It is because other initializations functinos are named as
    "init-...".

--- src/input.lisp
+++ src/input.lisp
@@ -28,7 +28,7 @@
            :touch-event-element-x
            :touch-event-element-y
 
-           :initialize-input))
+           :init-input))
 (in-package :cl-web-2d-game.input)
 
 (enable-ps-experiment-syntax)
@@ -206,7 +206,7 @@ device-state = boolean-value"
 
 ;; register
 
-(defun.ps initialize-input ()
+(defun.ps init-input ()
   (window.add-event-listener "mousemove" on-mouse-move-event)
   (window.add-event-listener "mousedown" on-mouse-down-event)
   (window.add-event-listener "mouseup" on-mouse-up-event)
