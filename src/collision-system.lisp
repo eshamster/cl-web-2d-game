c64faa4ff337f57497fa1fe1da95c7e7f2a996ae
eshamster <hamgoostar@gmail.com>
2017-09-25 06:29:58 +0000

    Update collision system to enable to add collider model

--- src/collision-system.lisp
+++ src/collision-system.lisp
@@ -7,13 +7,73 @@
         :parenscript
         :cl-web-2d-game.basic-components
         :cl-web-2d-game.collision
+        :cl-web-2d-game.2d-geometry
+        :cl-web-2d-game.draw-model-system
         :cl-web-2d-game.performance)
   (:export :collision-system
-           :make-collision-system))
+           :make-collision-system
+           :setf-collider-model-enable))
 (in-package :cl-web-2d-game.collision-system)
 
 (enable-ps-experiment-syntax)
 
+(defvar.ps+ *collider-model-color* #x00ff00)
+(defvar.ps+ *collider-model-depth* 10)
+(defvar.ps+ *collider-model-enable* t)
+
+(defun.ps+ generate-collider-model (physic-2d)
+  (flet ((make-a-model (fn-make-geometry)
+           (make-model-2d :model (funcall fn-make-geometry *collider-model-color*)
+                          :depth *collider-model-depth*
+                          :offset (clone-point-2d (physic-2d-offset physic-2d)))))
+    (etypecase physic-2d
+      (physic-circle (make-a-model
+                      (lambda (color)
+                        (make-wired-circle
+                         :color color
+                         :r (physic-circle-r physic-2d)))))
+      (physic-triangle (make-a-model
+                        (lambda (color)
+                          (make-wired-polygon
+                           :color color
+                           :pnt-list (mapcar (lambda (pnt)
+                                               (list (vector-2d-x pnt)
+                                                     (vector-2d-y pnt)))
+                                             (list (physic-triangle-pnt1 physic-2d)
+                                                   (physic-triangle-pnt2 physic-2d)
+                                                   (physic-triangle-pnt3 physic-2d)))))))
+      (physic-polygon (make-a-model
+                       (lambda (color)
+                         (make-wired-polygon
+                          :color color
+                          :pnt-list (mapcar (lambda (pnt)
+                                              (list (vector-2d-x pnt)
+                                                    (vector-2d-y pnt)))
+                                            (physic-polygon-pnt-list physic-2d)))))))))
+
+(defun.ps+ add-collider-model (entity)
+  ;; TODO: Avoid duplicated addition
+  (register-next-frame-func
+   (lambda ()
+     (with-ecs-components (physic-2d) entity
+       (add-ecs-component (generate-collider-model physic-2d) entity
+                          physic-2d)))))
+
+(defun.ps+ setf-collider-model-enable (value)
+  (unless (eq value *collider-model-enable*)
+    (setf *collider-model-enable* value)
+    (do-ecs-entities entity
+      (do-ecs-components-of-entity (comp entity
+                                         :component-type 'physic-2d)
+        (let ((model (find-a-component (lambda (target) (typep target 'model-2d))
+                                       comp)))
+          (when model
+            (if value
+                (enable-model-2d entity :target-model-2d model)
+                (disable-model-2d entity :target-model-2d model))))))
+    t))
+
+;; TODO: Don't make collider model when *collider-model-enable* is nil
 (defstruct.ps+
     (collision-system
      (:include ecs-system
@@ -29,4 +89,5 @@
                                  (loop for inner-idx from (1+ outer-idx) below length do
                                       (let ((entity2 (aref entities inner-idx)))
                                         (with-ecs-components ((ph2 physic-2d)) entity2
-                                          (process-collision entity1 ph1 entity2 ph2)))))))))))))))
+                                          (process-collision entity1 ph1 entity2 ph2))))))))))))
+               (add-entity-hook #'add-collider-model))))
