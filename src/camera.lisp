db5364022e2177fccd17822743c94a015c8ab245
eshamster <hamgoostar@gmail.com>
2018-01-04 13:42:46 +0000

    Add getter of screen width and height

--- src/camera.lisp
+++ src/camera.lisp
@@ -8,6 +8,8 @@
                 :with-slots-pair)
   (:export :get-camera-offset-x
            :get-camera-offset-y
+           :get-screen-width
+           :get-screen-height
            :init-camera))
 (in-package :cl-web-2d-game.camera)
 
@@ -15,11 +17,18 @@
 
 (defvar.ps+ *camera-offset-x* 0)
 (defvar.ps+ *camera-offset-y* 0)
+(defvar.ps+ *screen-width* 0)
+(defvar.ps+ *screen-height* 0)
 
 (defun.ps+ get-camera-offset-x ()
   *camera-offset-x*)
 (defun.ps+ get-camera-offset-y ()
   *camera-offset-y*)
+
+(defun.ps+ get-screen-width ()
+  *screen-width*)
+(defun.ps+ get-screen-height ()
+  *screen-height*)
 
 (defun.ps init-camera (offset-x offset-y width height)
   (let* ((x offset-x)
@@ -31,5 +40,7 @@
                        0 (* z 2)))))
     (setf *camera-offset-x* offset-x)
     (setf *camera-offset-y* offset-y)
+    (setf *screen-width* width)
+    (setf *screen-height* height)
     (camera.position.set 0 0 z)
     camera))
