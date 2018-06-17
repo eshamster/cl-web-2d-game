f62903d14650a1545176db8b32c434127cea0bfc
eshamster <hamgoostar@gmail.com>
2018-06-17 05:17:09 +0000

    Add def-game-state

--- core/game-state.lisp
+++ core/game-state.lisp
@@ -10,7 +10,10 @@
            :game-state-process
            :process
            :game-state-end-process
-           :end-process)
+           :end-process
+
+           :make-state
+           :def-game-state)
   (:import-from :cl-ps-ecs
                 :process))
 (in-package :cl-web-2d-game/core/game-state)
@@ -48,3 +51,37 @@
   (check-type state game-state)
   (setf *current-game-state* state
         *current-sub-game-state* :start))
+
+;; --- experimental definition macro --- ;
+
+(defvar.ps+ *state-maker-table* (make-hash-table))
+
+(defun.ps+ make-state (kind &rest keys)
+  "Make game state instance.
+Please see also the document of the def-game-state for detail."
+  (apply (gethash kind *state-maker-table*) keys))
+
+(defun.ps+ register-state-maker (kind func)
+  (setf (gethash kind *state-maker-table*) func))
+
+(defmacro def-game-state (name (&rest params) &key start-process process end-process)
+  "Define game state. This is mainly a wrapper of definition of a struct inheriting game-state.
+You can create an instance by (make-state :name).
+The \"params\" is same to the slot definition of defstruct.
+Ex. (def-game-state name (param1 (param2 100))) -> (make-state :name :param1 999)
+And you can access to the parameter by, for example, (slot-value <instance> 'param1)."
+  `(progn
+     (defstruct.ps+
+         (,(intern (format nil "GAME-~A-STATE" name))
+           (:include game-state
+                     ,@(append (when start-process
+                                 `((start-process ,start-process)))
+                               (when process
+                                 `((process ,process)))
+                               (when end-process
+                                 `((end-process ,end-process))))))
+         ,@params)
+     (def-top-level-form.ps+ ,(intern (format nil "GAME-STATE-REGISTER-~A" name))
+       (register-state-maker
+        ,(intern (symbol-name name) (find-package "KEYWORD"))
+        (function ,(intern (format nil "MAKE-GAME-~A-STATE" name)))))))
