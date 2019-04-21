65ae5cc835598cb0b67f8abfa3765aa88f514779
eshamster <hamgoostar@gmail.com>
2019-04-21 10:12:17 +0000

    Update: Enable stage-generator to include other defined element

--- utils/stage-generator.lisp
+++ utils/stage-generator.lisp
@@ -17,19 +17,28 @@
 
 (defstruct.ps+ element (time 0) (func (lambda ())))
 
+(defstruct.ps+ interpreter func include-list)
+
 (defstruct.ps+ stage (current-time 0) (element-list (list)))
 
 ;; --- export --- ;;
 
-(defmacro.ps+ def-stage-element-interpreter.ps+ (name lambda-key-form &body body)
-  (with-gensyms (stage args)
-    `(def-top-level-form.ps+ ,(symbolicate '_def-stage-element-interpreter_ name)
-       (register-stage-element-interpreter
-        ,name (lambda (,stage ,args)
-                (apply (lambda (&key time ,@lambda-key-form)
-                       (add-element-to-stage ,stage time
-                                             (lambda () ,@body)))
-                      ,args))))))
+(defmacro.ps+ def-stage-element-interpreter.ps+
+    (name-and-options lambda-key-form &body body)
+  (multiple-value-bind (name include)
+      (parse-name-and-options name-and-options)
+    (with-gensyms (stage args immediate-p)
+      `(def-top-level-form.ps+ ,(symbolicate '_def-stage-element-interpreter_ name)
+         (register-stage-element-interpreter
+          ,name (lambda (,stage ,args ,immediate-p)
+                  (apply (lambda ,(generate-lambda-list
+                                   lambda-key-form include)
+                           (if ,immediate-p
+                               (progn ,@body)
+                               (add-element-to-stage ,stage time
+                                                     (lambda () ,@body))))
+                         ,args))
+          ',include)))))
 
 (defmacro.ps+ generate-stage (&body defs)
   (with-gensyms (result-stage)
@@ -59,6 +68,20 @@
 ;; --- internal --- ;;
 
 (eval-when (:execute :load-toplevel :compile-toplevel)
+  (defun parse-name-and-options (name-and-options)
+    "Return (values name include-list)"
+    (if (listp name-and-options)
+        (let* ((name (car name-and-options))
+               (options (cdr name-and-options))
+               (include-list (cdr (assoc :include options))))
+          (values name include-list))
+        name-and-options))
+
+  (defun generate-lambda-list (lambda-key-form include-keyword-list)
+    `(&key time ,@lambda-key-form
+           ,@(mapcar #'symbolicate include-keyword-list))))
+
+(eval-when (:execute :load-toplevel :compile-toplevel)
   (defun.ps+ add-element-to-stage (stage time func)
     (with-slots (element-list) stage
       (push (make-element :time time :func func)
@@ -70,18 +93,40 @@
 
   (defvar.ps+ *stage-element-interpreter-table* (make-hash-table))
 
-  (defun.ps+ register-stage-element-interpreter (name func)
-    (setf (gethash name *stage-element-interpreter-table*)
-          func))
+  (defun.ps+ get-stage-element-interpreter-table ()
+    *stage-element-interpreter-table*)
 
-  (defun.ps+ interpret-element (stage kind &rest def)
-    (let ((func (gethash kind *stage-element-interpreter-table*)))
-      (funcall func stage def)))
+  (defun.ps+ register-stage-element-interpreter (name func include-list)
+    (setf (gethash name (get-stage-element-interpreter-table))
+          (make-interpreter :func func
+                            :include-list include-list)))
+
+  (defun.ps+ interpret-element%% (stage kind args immediate-p)
+    (let ((interpreter (gethash kind (get-stage-element-interpreter-table))))
+      (funcall (interpreter-func interpreter) stage args immediate-p)))
+
+  (defun interpret-element% (stage kind def-args &optional (immediate-p nil))
+    (flet ((include-args-p (arg)
+             (and (listp arg) (keywordp (car arg))))
+           (extract-if (predicate sequence)
+             (remove-if (lambda (elem) (not (funcall predicate elem)))
+                        sequence)))
+      (let* ((normal-args (remove-if #'include-args-p def-args))
+             (include-args (extract-if #'include-args-p def-args))
+             (interpreted-include-args
+              (mapcan (lambda (args)
+                        (let ((key (car args)))
+                          (list key (interpret-element% stage key (cdr args) t))))
+                      include-args)))
+        `(interpret-element%% ,stage ,kind
+                              (append (list ,@normal-args)
+                                      (list ,@interpreted-include-args))
+                              ,immediate-p))))
 
   (defun.ps+ generate-stage% (result-stage def-list)
     (mapcar (lambda (def)
               (if (keywordp (car def))
-                  `(interpret-element ,result-stage ,@def)
+                  (interpret-element% result-stage (car def) (cdr def))
                   `(macrolet ((stage (&body body)
                                 `(progn ,@(generate-stage% ',result-stage body))))
                      ,def)))
